49d6bcc086f34908bf81b3797fc5717d08817bfc
Lucas Takejame <lucas@takejame.com>
2019-03-17 19:06:48 -0300

    Added logging macro

--- Source/portifolio/DebugUtils.h
+++ Source/portifolio/DebugUtils.h
@@ -8,3 +8,4 @@
 #define logVec(vec) UE_LOG(LogTemp, Warning, TEXT("%s [%d] : %s -> (%f,%f,%f)"), TEXT(__FUNCTION__), __LINE__, TEXT(str(vec)),vec.X, vec.Y, vec.Z)
 #define logFStr(text) UE_LOG(LogTemp, Warning, TEXT("%s [%d] : %s"), TEXT(__FUNCTION__), __LINE__, *text)
 #define log(text) UE_LOG(LogTemp, Warning, TEXT("%s [%d] : %s"), TEXT(__FUNCTION__), __LINE__, TEXT(text))
+#define lg(text, ...) UE_LOG(LogTemp, Warning, TEXT(text), ##__VA_ARGS__)
